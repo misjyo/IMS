bbafe718a954f7a313d9a651a69d1bba92ea5b6e
Jyoti Mishra <mishrajyoti356@gmail.com>
2026-02-06 17:01:00 +0530

    chore: update backend baseUrl for deployment

--- frontend/src/store/apiSlice.js
+++ frontend/src/store/apiSlice.js
@@ -3,7 +3,7 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
 export const apiSlice = createApi({
     reducerPath: 'api',
     baseQuery: fetchBaseQuery({
-        // baseUrl: 'http://localhost:5000/api',
+      
         baseUrl: 'https://ims-ol63.onrender.com/',
         prepareHeaders: (headers) => {
             const token = localStorage.getItem('token');
