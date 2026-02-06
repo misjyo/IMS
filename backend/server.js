83805d4eb63159d9222ed247677fe033e0d0a520
Jyoti Mishra <mishrajyoti356@gmail.com>
2026-02-06 16:44:30 +0530

    chore: update backend baseUrl for deployment

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,15 @@ connectDB();
 const app = express();
 
 // Middlewares
-app.use(cors()); 
+const corsOptions = {
+    origin: ['https://ims-f0atnirws-misjyos-projects.vercel.app', 'http://localhost:5173'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+};
+app.use(cors(corsOptions));
+
+// app.use(cors()); 
 app.use(express.json()); 
 
 // Route Middleware Pipeline
